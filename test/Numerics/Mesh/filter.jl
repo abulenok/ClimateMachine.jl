eeb43598751be66dd5e3b0dd4e9e8db95772a970
sandreza <andrenogueirasouza@gmail.com>
2021-03-27 22:18:38 -0400

    move test to filter.jl

--- test/Numerics/Mesh/filter.jl
+++ test/Numerics/Mesh/filter.jl
@@ -397,3 +397,136 @@ end
         end
     end
 end
+
+function cubedshellwarp(a, b, c, R = max(abs(a), abs(b), abs(c)))
+
+    function f(sR, ξ, η)
+        X, Y = tan(π * ξ / 4), tan(π * η / 4)
+        x1 = sR / sqrt(X^2 + Y^2 + 1)
+        x2, x3 = X * x1, Y * x1
+        x1, x2, x3
+    end
+
+    fdim = argmax(abs.((a, b, c)))
+    if fdim == 1 && a < 0
+        # (-R, *, *) : Face I from Ronchi, Iacono, Paolucci (1996)
+        x1, x2, x3 = f(-R, b / a, c / a)
+    elseif fdim == 2 && b < 0
+        # ( *,-R, *) : Face II from Ronchi, Iacono, Paolucci (1996)
+        x2, x1, x3 = f(-R, a / b, c / b)
+    elseif fdim == 1 && a > 0
+        # ( R, *, *) : Face III from Ronchi, Iacono, Paolucci (1996)
+        x1, x2, x3 = f(R, b / a, c / a)
+    elseif fdim == 2 && b > 0
+        # ( *, R, *) : Face IV from Ronchi, Iacono, Paolucci (1996)
+        x2, x1, x3 = f(R, a / b, c / b)
+    elseif fdim == 3 && c > 0
+        # ( *, *, R) : Face V from Ronchi, Iacono, Paolucci (1996)
+        x3, x2, x1 = f(R, b / c, a / c)
+    elseif fdim == 3 && c < 0
+        # ( *, *,-R) : Face VI from Ronchi, Iacono, Paolucci (1996)
+        x3, x2, x1 = f(-R, b / c, a / c)
+    else
+        error("invalid case for cubedshellwarp: $a, $b, $c")
+    end
+
+    return x1, x2, x3
+end
+
+@testset "Mass Preserving Cutoff Filter Conservation Test" begin
+    N = 3
+    Ne = (1, 1, 1)
+    dim = 3
+    # dim, direction
+    @testset for FT in (Float64, )
+        Rrange = [FT(1.0), FT(1.2)]
+
+        topl = ClimateMachine.Mesh.Grids.StackedCubedSphereTopology(
+            MPI.COMM_WORLD,
+            1,
+            Rrange,
+            boundary = (5,6), 
+        )
+
+        grid = ClimateMachine.Mesh.Grids.DiscontinuousSpectralElementGrid(
+            topl,
+            FloatType = FT,
+            DeviceArray = ClimateMachine.array_type(),
+            polynomialorder = (N, N),
+            meshwarp = cubedshellwarp,
+        )
+
+
+        mp_filter = ClimateMachine.Mesh.Filters.MassPreservingCutoffFilter(
+            grid,
+            2,
+        )
+
+        reg_filter = ClimateMachine.Mesh.Filters.CutoffFilter(
+            grid,
+            2,
+        )
+
+        model = FilterTestModel{4}()
+        dg = ClimateMachine.DGMethods.DGModel(
+            model,
+            grid,
+            nothing,
+            nothing,
+            nothing;
+            state_gradient_flux = nothing,
+        )
+
+        # test mp filter
+        filter = mp_filter
+        Q = ClimateMachine.DGMethods.init_ode_state(
+                dg,
+                nothing,
+                dim,
+        )
+        sum_before_1 = weightedsum(Q, 1)
+        sum_before_2 = weightedsum(Q, 2)
+        sum_before_3 = weightedsum(Q, 3)
+        target = 1:3
+        ClimateMachine.Mesh.Filters.apply!(
+            Q,
+            target,
+            grid,
+            filter,
+        )
+        sum_after_1 = weightedsum(Q, 1)
+        sum_after_2 = weightedsum(Q, 2)
+        sum_after_3 = weightedsum(Q, 3)
+
+        @test sum_before_1 ≈ sum_after_1
+        @test sum_before_2 ≈ sum_after_2
+        @test sum_before_3 ≈ sum_after_3
+
+
+        # test regular filter
+        filter = reg_filter
+        Q = ClimateMachine.DGMethods.init_ode_state(
+                dg,
+                nothing,
+                dim,
+        )
+        sum_before_1 = weightedsum(Q, 1)
+        sum_before_2 = weightedsum(Q, 2)
+        sum_before_3 = weightedsum(Q, 3)
+        target = 1:3
+        ClimateMachine.Mesh.Filters.apply!(
+            Q,
+            target,
+            grid,
+            filter,
+        )
+        sum_after_1 = weightedsum(Q, 1)
+        sum_after_2 = weightedsum(Q, 2)
+        sum_after_3 = weightedsum(Q, 3)
+
+        @test !(sum_before_1 ≈ sum_after_1)
+        @test !(sum_before_2 ≈ sum_after_2)
+        @test !(sum_before_3 ≈ sum_after_3)
+
+    end
+end
